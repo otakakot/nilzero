80493f52cd434d138f52a94a90b1b5722a007d79
otakakot <kotaro.otaka@gmail.com>
2024-04-06 00:43:31 +0900

    first commit

--- nilzero.go
+++ nilzero.go
@@ -1,5 +1,6 @@
 package nilzero
 
+// NilZero returns the zero value of the type of the pointer v if v is nil, otherwise it returns the value of v.
 func NilZero[T any](v *T) T {
 	if v == nil {
 		var zero T
